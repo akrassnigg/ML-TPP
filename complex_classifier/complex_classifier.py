111663ce4eef7f0708ffd0b8b00a134b3b99a8e7
Andreas Krassnigg <57106033+akrassnigg@users.noreply.github.com>
2021-04-10 20:39:52 +0200

    Made training data generation and a binary classifier work.

--- complex_classifier/complex_classifier.py
+++ complex_classifier/complex_classifier.py
@@ -26,6 +26,8 @@ import pytorch_lightning as pl
 from pytorch_lightning.core import LightningModule
 from pytorch_lightning.loggers import TensorBoardLogger
 
+import pytorch_lightning.metrics.utils as ptlmu
+
 
 logdir = "logs"
 
@@ -41,7 +43,27 @@ class RandomClasses(Dataset):
 
     def __len__(self):
         num_of_data_points = len(self.data_X)
-        print("Successfully loaded training data of length: ", num_of_data_points)
+        print("Successfully loaded random training data of length: ", num_of_data_points)
+        return num_of_data_points
+
+    def __getitem__(self, idx):
+        return self.data_X[idx], self.data_y[idx]
+    
+
+
+
+class PoleClasses(Dataset):
+    def __init__(self, data_dir):
+
+        self.data_X = np.load(data_dir+"/various_poles_data_classifier_x.npy", allow_pickle=True).astype('float32')
+        self.data_y = np.reshape(np.load(data_dir+"/various_poles_data_classifier_y.npy", allow_pickle=True).astype('int'), (-1,1))
+        
+        print("Checking shape of loaded data: X: ", np.shape(self.data_X))
+        print("Checking shape of loaded data: y: ", np.shape(self.data_y))
+
+    def __len__(self):
+        num_of_data_points = len(self.data_X)
+        print("Successfully loaded pole training data of length: ", num_of_data_points)
         return num_of_data_points
 
     def __getitem__(self, idx):
@@ -202,30 +224,212 @@ class DeepNet1(LightningModule):
     #     return parser
 
 
+class PoleClassifier(LightningModule):
+    """
+    Basic lightning model to use a vector of inputs in order to predict
+    the class of a complex structure in the vector
+    """
+
+    def __init__(self,
+                 in_features: int = 64,
+                 hidden_dim: int = 1000,
+                 out_features: int = 2,
+                 drop_prob: float = 0.2,
+                 learning_rate: float = 0.001,
+                 batch_size: int = 20,
+                 val_batch_size: int = 2,
+                 test_batch_size: int = 2,
+                 validation_portion: float = .2,
+                 test_portion: float = .2,
+                 data_root: str = './data',
+                 num_data: int = 200,
+                 num_workers: int = 0,
+                 **kwargs
+                 ):
+        # init superclass
+        super().__init__()
+        # save all variables in __init__ signature to self.hparams
+        self.save_hyperparameters()
+
+        self.fc_1 = nn.Linear(in_features=self.hparams.in_features,
+                              out_features=self.hparams.hidden_dim)
+        self.fc_1_bn = nn.BatchNorm1d(self.hparams.hidden_dim)
+        self.fc_1_drop = nn.Dropout(self.hparams.drop_prob)
+
+        self.fc_2 = nn.Linear(in_features=self.hparams.hidden_dim,
+                              out_features=self.hparams.hidden_dim)
+
+        self.fc_3 = nn.Linear(in_features=self.hparams.hidden_dim,
+                              out_features=self.hparams.hidden_dim)
+
+        self.fc_4 = nn.Linear(in_features=self.hparams.hidden_dim,
+                              out_features=self.hparams.out_features)
+
+        self.example_input_array = torch.zeros(20, self.hparams.in_features)
+        
+        
+    def forward(self, x):
+        """
+        Just a simple net at the moment
+        """
+        # print("Testing the shape of the input: ", x.size())
+        x = self.fc_1(x.view(-1, self.hparams.in_features))
+        x = torch.relu(x)
+        x = self.fc_1_bn(x)
+        x = self.fc_1_drop(x)
+        x = self.fc_2(x)
+        x = torch.relu(x)
+        x = self.fc_1_bn(x)
+        x = self.fc_1_drop(x)
+        x = self.fc_3(x)
+        x = torch.relu(x)
+        x = self.fc_1_bn(x)
+        x = self.fc_1_drop(x)
+        x = self.fc_4(x)
+        # x = torch.argmax(x, dim=1)
+        return x
+
+    def training_step(self, batch, batch_idx):
+
+        x, y = batch
+        y_hat = self(x).unsqueeze(-1)
+        # print("y: ", y)
+        # print("y_hat: ", y_hat)
+        
+        loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss, on_step=False, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+
+        x, y = batch
+        y_hat = self(x).unsqueeze(-1)
+        # print("y: ", y)
+        # print("y_hat: ", y_hat)
+                
+        val_loss = F.cross_entropy(y_hat, y)
+        self.log('val_loss', val_loss, on_step=False, on_epoch=True, prog_bar=True)
+        return val_loss
+
+    def test_step(self, batch, batch_idx):
+
+        x, y = batch
+        y_hat = self(x).unsqueeze(-1)
+        # print("y: ", y)
+        # print("y_hat: ", y_hat)
+        
+        test_loss = F.cross_entropy(y_hat, y)
+        self.log('test_loss', test_loss, on_step=False, on_epoch=True)
+        return test_loss
+
+
+    # ---------------------
+    # TRAINING SETUP
+    # ---------------------
+    def configure_optimizers(self):
+        """
+        Return whatever optimizers and learning rate schedulers you want here.
+        At least one optimizer is required.
+        """
+        optimizer = optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        # scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
+        # return [optimizer], [scheduler]
+        return optimizer
+
+
+    def setup(self, stage):
+        all_data = PoleClasses(self.hparams.data_root)
+        
+        print("Length of all_data: ", len(all_data))
+        print("Length of data hyperparameter: ", self.hparams.num_data)
+        
+        self.validation_number = int(self.hparams.num_data * self.hparams.validation_portion)
+        self.test_number = int(self.hparams.num_data * self.hparams.test_portion)
+        self.training_number = self.hparams.num_data - self.test_number - self.validation_number
+        
+        print("Data splits: ", self.training_number, self.validation_number, self.test_number, self.hparams.num_data)
+
+        train_part, val_part, test_part = random_split(all_data, [self.training_number, self.validation_number, self.test_number])
+
+        self.train_dataset = train_part
+        self.val_dataset = val_part
+        self.test_dataset = test_part
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.hparams.batch_size, num_workers=self.hparams.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.hparams.val_batch_size, num_workers=self.hparams.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.hparams.test_batch_size, num_workers=self.hparams.num_workers)
+
+    # @staticmethod
+    # def add_model_specific_args(parent_parser, root_dir):  # pragma: no-cover
+    #     """
+    #     Define parameters that only apply to this model
+    #     """
+    #     parser = ArgumentParser(parents=[parent_parser])
+
+    #     # param overwrites
+    #     # parser.set_defaults(gradient_clip_val=5.0)
+
+    #     # network params
+    #     parser.add_argument('--in_features', default=28 * 28, type=int)
+    #     parser.add_argument('--hidden_dim', default=50000, type=int)
+    #     # use 500 for CPU, 50000 for GPU to see speed difference
+    #     parser.add_argument('--out_features', default=10, type=int)
+    #     parser.add_argument('--drop_prob', default=0.2, type=float)
+
+    #     # data
+    #     parser.add_argument('--data_root', default=os.path.join(root_dir, 'mnist'), type=str)
+    #     parser.add_argument('--num_workers', default=4, type=int)
+
+    #     # training params (opt)
+    #     parser.add_argument('--epochs', default=20, type=int)
+    #     parser.add_argument('--batch_size', default=64, type=int)
+    #     parser.add_argument('--learning_rate', default=0.001, type=float)
+    #     return parser
+
+
 if __name__ == '__main__':
 
             
     # locally, for testing, debugging, etc.: fast_dev_run=True, 
-    trainer = pl.Trainer(max_epochs=30, logger=tb_logger)
+    trainer = pl.Trainer(max_epochs=300, logger=tb_logger)
 
     # on production machimne
     # trainer = pl.Trainer(gpus=1, distributed_backend='dp', max_epochs=30, logger=tb_logger, log_gpu_memory='all')
 
 
-    # run the training, etc., including validation on part of the training data set:
-    model = DeepNet1(in_features = 20,
+    # # run the training, etc., including validation on part of the training data set on random test data:
+    # model = DeepNet1(in_features = 20,
+    #              hidden_dim = 100,
+    #              out_features = 4,
+    #              drop_prob = 0.2,
+    #              learning_rate = 0.001 * 8,
+    #              batch_size = 200,
+    #              val_batch_size = 40,
+    #              test_batch_size = 40,
+    #              validation_portion = .2,
+    #              test_portion = .2,
+    #              data_root = './data',
+    #              num_data = 200,
+    #              num_workers = 0)
+                 
+    model = PoleClassifier(in_features = 128,
                  hidden_dim = 100,
-                 out_features = 4,
-                 drop_prob = 0.2,
-                 learning_rate = 0.001 * 8,
-                 batch_size = 200,
-                 val_batch_size = 40,
-                 test_batch_size = 40,
+                 out_features = 2,
+                 drop_prob = 0.3,
+                 learning_rate = 0.001,
+                 batch_size = 20,
+                 val_batch_size = 10,
+                 test_batch_size = 10,
                  validation_portion = .2,
                  test_portion = .2,
                  data_root = './data',
-                 num_data = 200,
-                 num_workers = 8)
+                 num_data = 20000,
+                 num_workers = 0)
                  
     trainer.fit(model)
     trainer.test(model)
--- complex_classifier/training_data_generation.py
+++ complex_classifier/training_data_generation.py
@@ -12,15 +12,21 @@ Complex singluarity data generation code
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 
-n_examples = 200
+n_examples = 20000
+
+the_scale = 10.
+
+standard_re = np.linspace(0.01, 20., num=128)
+standard_im = np.linspace(0., 0., num=128)
 
 def single_complex_pole(list_re, list_im, part_re, part_im, power=1, coeff=1.):
     """
     Computes the function values on a list of complex coordinates of a
-    single singularity of power power, with a multiplicative coefficient coeff
+    single singularity of power power, with a multiplicative complex coefficient coeff
     and a position in the complex plane given by part_re and part_im
     
     """
@@ -36,6 +42,39 @@ def single_complex_pole(list_re, list_im, part_re, part_im, power=1, coeff=1.):
     return result_re, result_im
 
 
+def single_real_pole(list_re, part_re, power=1, coeff=1.):
+    """
+    Computes the function values on a list of coordinates on the real axis of a
+    single singularity of power power, with a multiplicative coefficient coeff
+    and a position on the real axis given by part_re 
+    
+    """
+    
+    len_set = len(list_re)
+    list_im = np.linspace(0., 0., num=len_set)
+    
+    result_re, _ = single_complex_pole(list_re, list_im, part_re, 0., power=power, coeff=coeff)
+    
+    return result_re
+
+
+def complex_conjugate_pole_pair(list_re, part_re, part_im, power=1, coeff=1.):
+    """
+    Computes the function values on a list of coordinates on the real axis of a
+    single singularity of power power, with a multiplicative coefficient coeff
+    and a position on the real axis given by part_re 
+    
+    """
+    
+    len_set = len(list_re)
+    list_im = np.linspace(0., 0., num=len_set)
+    
+    result_re_plus, _ = single_complex_pole(list_re, list_im, part_re, part_im, power=power, coeff=coeff)
+    result_re_minus, _ = single_complex_pole(list_re, list_im, part_re, - part_im, power=power, coeff=np.conjugate(coeff))
+    
+    return result_re_plus + result_re_minus
+
+
 def list_of_random_vectors(n_vectors, length_vectors):
     return np.reshape(np.random.random(size=n_vectors*length_vectors), (n_vectors, length_vectors))
 
@@ -48,9 +87,71 @@ def list_of_random_vectors(n_vectors, length_vectors):
 
 # create and save testing training data
 
-test_random_data_x = list_of_random_vectors(n_examples, 20)
-test_random_data_y = np.random.randint(0,high=3, size=n_examples)
+
+def create_training_data(length=n_examples):
+    
+    # # Random example numbers
+    # test_random_data_x = list_of_random_vectors(n_examples, 20)
+    # test_random_data_y = np.random.randint(0,high=3, size=n_examples)
+    
+    
+    # np.save("random_test_data_classifier_x.npy", test_random_data_x)
+    # np.save("random_test_data_classifier_y.npy", test_random_data_y)
+
+    data_x = []
+    data_y = []
+    
+    for counter in range(n_examples):
+        label = np.random.choice([0,1])
+        
+        if label == 0:
+            # a single pole on the real axis
+            
+            part_re = the_scale * np.random.random()
+            
+            out_re = single_real_pole(standard_re, part_re)
+            
+        elif label == 1:
+            # a complex conjugated pole pair
+            
+            part_re = the_scale * 2 * (np.random.random() - 0.5)
+            part_im = the_scale * 2 * (np.random.random() - 0.5)
+            
+            out_re = complex_conjugate_pole_pair(standard_re, part_re, part_im)
+            
+            
+        else:
+            # should not happen
+            sys.exit("Undefined label.")
+        
+        data_x.append(out_re)
+        data_y.append(label)
+
+    np.save("various_poles_data_classifier_x.npy", data_x)
+    np.save("various_poles_data_classifier_y.npy", data_y)
+
+    print("Successfully saved x data of shape ", np.shape(data_x))
+    print("Successfully saved y data of shape ", np.shape(data_y))
+
+    return
 
 
-np.save("random_test_data_classifier_x.npy", test_random_data_x)
-np.save("random_test_data_classifier_y.npy", test_random_data_y)
+if __name__ == '__main__':
+
+    # out_re, out_im = single_complex_pole(standard_re, standard_im, 1.3, 0.)     
+
+    # print("Re: ", out_re)
+    # print("Im: ", out_im)
+
+
+    create_training_data(length=n_examples)
+
+
+
+
+
+
+
+
+
+#
